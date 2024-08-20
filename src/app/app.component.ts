d241bc44b39f12526db3fbd4b811691d779db99b
crimson666 <ankurbsws@gmail.com>
2024-08-21 02:31:56 +0530

    1st master push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnbWeb';
+export class AppComponent implements OnInit {
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+  ngOnInit():void {
+    this.initFrontAwesome();
+  }
+
+  private initFrontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,4 @@
+import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [ faAirbnb ];
