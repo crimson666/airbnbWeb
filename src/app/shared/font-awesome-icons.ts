5325a8e459059fd92a70259898cf59cdd3716ced
crimson666 <ankurbsws@gmail.com>
2024-08-21 12:28:58 +0530

    21/8/24 fontawesome and avatar stuff

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,4 +1,4 @@
 import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
-export const fontAwesomeIcons: IconDefinition[] = [ faAirbnb ];
+export const fontAwesomeIcons: IconDefinition[] = [ faAirbnb,faUser ];
