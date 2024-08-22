3232e8ca66b26ff1080c1bb66db7f7542774d019
crimson666 <ankurbsws@gmail.com>
2024-08-22 23:12:04 +0530

    Watning and toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,39 @@ import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import {ToastModule} from "primeng/toast";
+import { ToastService } from './layout/toast.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,FontAwesomeModule,ButtonModule,NavbarComponent,FooterComponent,ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
   isListingView = true;
+  toastService = inject(ToastService);
+  messegeService = inject(MessageService);
   ngOnInit():void {
     this.initFrontAwesome();
+    this.listenToastService();
   }
 
   private initFrontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private listenToastService(){
+    this.toastService.sendSub.subscribe({
+      next: newMessege => {
+        if(newMessege && newMessege.summary !== this.toastService.INIT_STATE){
+          this.messegeService.add(newMessege);
+        }
+      }
+    })
   }
 
 }
